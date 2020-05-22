f048d2c625da1a51ee9113931d3edeaf1bb0a22c
pperone <pperone@gmail.com>
2020-05-22 15:05:54 -0300

    Adding deployer and deployed  information

--- deplog.py
+++ deplog.py
@@ -1,5 +1,6 @@
 import os
 import time
+import datetime
 import re
 
 from slackclient import SlackClient
@@ -21,8 +22,14 @@ class Organization(base):
 
     channel = Column(String, primary_key=True)
     staging = Column(String)
+    s_deployer = Column(String)
+    s_deployed = Column(String)
     feature = Column(String)
+    f_deployer = Column(String)
+    f_deployed = Column(String)
     teammobile = Column(String)
+    t_deployer = Column(String)
+    t_deployed = Column(String)
 
 Session = sessionmaker(engine)  
 session = Session()
@@ -74,9 +81,13 @@ def parse_bot_commands(slack_events):
 def handle_event(org, event):
     response = None
     production = False
+    time = datetime.datetime.now().strftime("%b %d,%Y | %H:%M")
+
     title = event["attachments"][0]["title"]
     environment = event["attachments"][0]["fields"][0]["value"]
     branch = event["attachments"][0]["fields"][1]["value"]
+    deployer = event["attachments"][0]["fields"][2]["value"]
+
     s_icon = ':apple:'
     f_icon = ':apple:'
     t_icon = ':apple:'
@@ -84,10 +95,16 @@ def handle_event(org, event):
     if title.startswith('New version deployed'):
         if environment == 'staging':
             org.staging = branch
+            org.s_deployer = deployer
+            org.s_deployed = time
         elif environment == 'feature':
             org.feature = branch
+            org.f_deployer = deployer
+            org.f_deployed = time
         elif environment == 'teammobile':
             org.teammobile = branch
+            org.t_deployer = deployer
+            org.t_deployed = time
         elif environment == 'production':
             production = True
 
@@ -100,7 +117,9 @@ def handle_event(org, event):
         if org.teammobile == 'develop':
             t_icon = ':green_apple:'
 
-        response = "\n \n" + s_icon + " *staging  |*  Current branch: *" + org.staging + "* \n\n " + f_icon + " *feature  |*  Current branch: *" + org.feature + "* \n\n " + t_icon + " *teammobile  |*  Current branch: *" + org.teammobile + "*"
+        response = "\n \n" + s_icon + " *staging  |*  Current branch: *" + org.staging + "*  |  Deployed by *" + org.s_deployer + "* on " + org.s_deployed + " \n\n "\
+                   + f_icon + " *feature  |*  Current branch: *" + org.feature + "*  |  Deployed by *" + org.f_deployer + "* on " + org.f_deployed + " \n\n "\
+                   + t_icon + " *teammobile  |*  Current branch: *" + org.teammobile + "*  |  Deployed by *" + org.t_deployer + "* on " + org.t_deployed
 
         if not production:
             if debugging:
