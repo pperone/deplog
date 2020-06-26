257fec9de2e3f5e26163bd14f5ef52ac6d58c98a
pperone <pperone@gmail.com>
2020-06-26 08:49:09 -0300

    Shortening output message

--- deplog.py
+++ deplog.py
@@ -121,9 +121,9 @@ def handle_event(org, event):
         if org.teammobile == 'develop' or org.teammobile.startswith('master_pre_production'):
             t_icon = ':green_apple:'
 
-        response = s_icon + " *staging    |*    Current branch: *" + org.staging + "    |*    Deployed by *" + org.s_deployer + "* on " + org.s_deployed + " \n\n"\
-                   + f_icon + " *feature    |*    Current branch: *" + org.feature + "    |*    Deployed by *" + org.f_deployer + "* on " + org.f_deployed + " \n\n"\
-                   + t_icon + " *teammobile    |*    Current branch: *" + org.teammobile + "    |*    Deployed by *" + org.t_deployer + "* on " + org.t_deployed
+        response = s_icon + " *staging   |*   Branch: *" + org.staging + "   |*   Deployed by *" + org.s_deployer + "* on " + org.s_deployed + " \n\n"\
+                   + f_icon + " *feature   |*   Branch: *" + org.feature + "   |*   Deployed by *" + org.f_deployer + "* on " + org.f_deployed + " \n\n"\
+                   + t_icon + " *teammobile   |*   Branch: *" + org.teammobile + "   |*   Deployed by *" + org.t_deployer + "* on " + org.t_deployed
 
         time.sleep(120)
 
